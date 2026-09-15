16535782bc0177d9f2f410efaea5d0ba0a49a033
dfsj <1609811258@qq.com>
2026-09-15 21:31:06 +0800

    perf: 加速课表首屏与背景资源加载

--- LessonTable/pages/index/index.vue
+++ LessonTable/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="page-root">
-	<image class="bgImg" src="/common/images/backgroundImg.png" />
+	<image class="bgImg" src="/common/images/backgroundImg.png?v=20260915" mode="aspectFill" :fade-show="false" />
 	<view class="bgMask"></view>
 	<view class="layout" :class="['mode-' + layoutMetrics.mode, animationEnabled ? 'anim-on' : 'anim-off']"
 		@click="handleGlobalTap()">
@@ -3001,6 +3001,8 @@ onUnmounted(() => {
 .page-root {
 	position: relative;
 	min-height: 100vh;
+	isolation: isolate;
+	background: linear-gradient(180deg, #a7b7ff 0%, #e5c4e8 34%, #ffd7b9 64%, #fff3d2 100%);
 }
 
 .bgImg {
@@ -3009,8 +3011,9 @@ onUnmounted(() => {
 	height: 100vh;
 	top: 0;
 	left: 0;
-	z-index: -1;
+	z-index: 0;
 	object-fit: cover;
+	pointer-events: none;
 }
 
 .bgMask {
@@ -3019,8 +3022,9 @@ onUnmounted(() => {
 	left: 0;
 	width: 100vw;
 	height: 100vh;
-	z-index: -1;
+	z-index: 1;
 	background: linear-gradient(180deg, rgba(249, 251, 255, 0.36) 0%, rgba(255, 255, 255, 0.16) 100%);
+	pointer-events: none;
 }
 
 .admin-debug-float {
